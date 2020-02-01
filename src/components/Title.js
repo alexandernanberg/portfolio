09c0bbd4ccdf86cfceff5436552f18d90edc1bdb
Alexander Nanberg <alex.nanberg@hotmail.com>
2020-02-01 11:47:37 +0100

    chore: fix styled-components compability

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,13 +1,14 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
-const Title = styled.h1`
+const baseStyle = css`
   margin-bottom: 0.2em;
   font-family: var(--font-family);
   font-weight: 700;
   line-height: 1.2em;
 `
 
-export const H1 = styled(Title)`
+export const H1 = styled.h1`
+  ${baseStyle}
   margin-bottom: 0.2em;
   font-size: 3rem;
 
@@ -16,7 +17,8 @@ export const H1 = styled(Title)`
   }
 `
 
-export const H3 = styled(Title.withComponent('h3'))`
+export const H3 = styled.h3`
+  ${baseStyle}
   font-size: 1.8rem;
   font-weight: 400;
   letter-spacing: 1px;
