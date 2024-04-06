00bffb25ec1b777dd55b016868f6b91573b328f0
Alexander Nanberg <alex.nanberg@hotmail.com>
2024-04-06 17:45:28 +0200

    New portfolio

---
+++ app/root.tsx
@@ -0,0 +1,38 @@
+import { LinksFunction } from '@remix-run/node'
+import {
+  Links,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from '@remix-run/react'
+import styles from './globals.css?url'
+
+export const links: LinksFunction = () => [{ rel: 'stylesheet', href: styles }]
+
+// TODO: meta author, twitter, og,
+
+export function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="Content-Language" content="en" />
+        <meta name="author" content="Alexander Nanberg" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        {children}
+        <ScrollRestoration />
+        <Scripts />
+      </body>
+    </html>
+  )
+}
+
+export default function App() {
+  return <Outlet />
+}
---
+++ app/routes/_index.tsx
@@ -0,0 +1,80 @@
+import type { MetaFunction } from '@remix-run/node'
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: 'Alexander Nanberg' },
+    {
+      name: 'description',
+      content:
+        'Software engineer with a passion for building delightful user experiences',
+    },
+  ]
+}
+
+export default function Index() {
+  return (
+    <div className="max-w-[620px] mx-auto p-4 my-20">
+      <header className="mb-16 flex gap-3 items-center">
+        <svg
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className="size-9 text-white"
+        >
+          <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M9.2802 2.58022C7.49094 3.4529 6.74791 5.61083 7.62059 7.40009L13.9411 20.3591C14.8138 22.1483 16.9717 22.8914 18.761 22.0187C20.5502 21.146 21.2933 18.9881 20.4206 17.1988L14.1001 4.23983C13.2274 2.45057 11.0695 1.70754 9.2802 2.58022Z"
+            fill="currentColor"
+          />
+          <path
+            d="M2.00049 18.7529C2.00049 20.7436 3.6143 22.3574 5.60503 22.3574C7.59577 22.3574 9.20958 20.7436 9.20958 18.7529C9.20958 16.7621 7.59577 15.1483 5.60503 15.1483C3.6143 15.1483 2.00049 16.7621 2.00049 18.7529Z"
+            fill="currentColor"
+          />
+        </svg>
+
+        <div className="flex flex-col gap-1">
+          <h1 className="font-medium leading-none">Alexander Nanberg</h1>
+          <span className="text-gray-400 block leading-none">
+            Senior Software Engineer
+          </span>
+        </div>
+      </header>
+      <p className="mt-3 text-gray-400">
+        As a software engineer, I craft delightful interfaces by blending
+        creativity with meticulous attention to detail, always aiming to push
+        the boundaries of user experience.
+      </p>
+
+      <h3 className="font-medium mt-12 mb-8">Connect</h3>
+      <div className="flex gap-4">
+        <a
+          href="https://github.com/alexandernanberg"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="underline text-gray-400 hover:text-white"
+        >
+          <span>GitHub</span> ↗
+        </a>
+        <a
+          href="https://www.linkedin.com/in/alexander-nanberg-5b9864115/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="underline text-gray-400 hover:text-white"
+        >
+          <span>LinkedIn</span> ↗
+        </a>
+        <a
+          href="https://dribbble.com/alexandernanberg"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="underline text-gray-400 hover:text-white"
+        >
+          <span>Dribble</span> ↗
+        </a>
+      </div>
+    </div>
+  )
+}
